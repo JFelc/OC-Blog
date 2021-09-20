3d9503cc992648f5e797fcf097ad68824aea9218
Julien <julien.feliciano@qgandco.fr>
2021-09-20 17:29:20 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+require 'vendor/autoload.php';
+Sentry\init(['dsn' => 'https://b6356c6b09f748bea81123e66343b99f@o1003778.ingest.sentry.io/5964378' ]);
+session_start();
+\Sentry\captureLastError();
+require_once "controller/controller.php";
+new Controller($_SERVER["REDIRECT_URL"],$_SERVER["QUERY_STRING"]);
+?>
