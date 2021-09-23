60fed3dc78d84500913e31951f1990d228f4918f
Julien <julien.feliciano@qgandco.fr>
2021-09-23 19:27:07 +0200

    Home & 404 init

---
+++ view/404.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>404 Page no found</title>
+	<meta name="viewport" content="width=device-width">
+    <link href="<?php echo $this->rewritebase; ?>public/assets/css/404.css" rel="stylesheet">
+</head>
+<body>
+	<div class="error-page-wrap">
+		<article class="error-page gradient">
+			<hgroup>
+				<h1>404</h1>
+				<h2>oops! page not found</h2>
+			</hgroup>
+			<a href="<?php echo $this->rewritebase; ?>" title="Back to site" class="error-back">back</a>
+		</article>
+	</div>
+</body>
+</html>
---
+++ view/home.php
@@ -0,0 +1,14 @@
+<main>
+
+  <section class="py-5 text-center container">
+    <div class="row py-lg-5">
+      <div class="col-lg-6 col-md-8 mx-auto">
+        <h1 class="fw-light">Julien Feliciano</h1>
+        <img class="col-6" src="<?php echo $this->rewritebase ?>uploads/image_prof.jpg">
+        <p class="lead text-muted">Développeur Front-End Junior</p>
+      </div>
+    </div>
+  </section>
+
+
+</main>
