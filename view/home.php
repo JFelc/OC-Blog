50be11477b6494d70b770278ee17660906fbbc41
Julien <julien.feliciano@qgandco.fr>
2021-10-09 15:31:40 +0200

    Dynamic views + css

---
+++ view/home.php
@@ -0,0 +1,63 @@
+<main>
+
+  <section class="py-5 text-center container">
+    <div class="row py-lg-5">
+      <div class="col-lg-6 col-md-8 mx-auto">
+        <h1 class="fw-light">Julien Feliciano</h1>
+        <img class="col-6" src="<?php echo $this->rewritebase ?>uploads/image_prof.jpg">
+        <p class="lead text-muted">Développeur Front-End Junior</p>
+      </div>
+    </div>
+  </section>
+
+<div class="container">
+<div class="row">
+  <?php foreach ($res as $key => $value) { ?>
+      <div class="col-4 mb-4">
+      <a href="<?php echo $this->rewritebase . 'post/' . $res[$key]['idPost']; ?>">
+        <div class="card">
+          <?php if (isset($res[$key]['photo']) && $res[$key]['photo'] != null) { ?>
+            <img src="<?php echo $this->rewritebase . $res[$key]['photo']; ?>">
+          <?php } else { ?>
+            <svg class="bd-placeholder-img card-img-top" width="100%" height="200" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Image cap" preserveAspectRatio="xMidYMid slice" focusable="false">
+              <rect width="100%" height="100%" fill="#868e96"></rect><text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text>
+            </svg>
+          <?php } ?>
+          <div class="card-body text-center">
+            <h3><?php echo $res[$key]['auteur'] ?></h3>
+            <h5 class="card-title"><?php echo $res[$key]['titre'] ?></h5>
+            <p class="card-text"><?php echo $res[$key]['description'] ?></p>
+          </div>
+        </div>
+        </a>
+      </div>
+  <?php  } ?>
+</div>
+</div>
+
+  <div class="album py-5 bg-light">
+    <div class="container">
+      <form>
+        <div class="form-group col-6 mx-auto">
+          <div class="row">
+            <div class="col-12 py-3">
+              <input type="text" class="form-control" placeholder="Nom">
+            </div>
+            <div class="col-12 py-3">
+              <input type="text" class="form-control" placeholder="Prénom">
+            </div>
+          </div>
+        </div>
+        <div class="form-group col-6 mx-auto py-3">
+          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Entrez l'adresse mail">
+        </div>
+        <div class=" form-group col-6 mx-auto">
+          <div class="text-end">
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+
+</main>
---
+++ view/profile.php
@@ -0,0 +1,44 @@
+<main class="align-middle form-signin">
+<?php if (isset($url[3]) && $url[3] == 'changePassword') { ?>
+<form method="POST" enctype="multipart/form-data">
+    <h1 class="h3 mb-3 fw-normal">Inscription</h1>
+   
+    <div class="form-floating">
+      <input type="password" name="oldPasswd" class="form-control" id="oldPassword" placeholder="Password">
+      <label for="oldPassword">Current Password</label>
+    </div>
+
+    <div class="form-floating">
+      <input type="password" name="newPasswd" class="form-control" id="newPassword" placeholder="Password">
+      <label for="newPassword">New Password</label>
+    </div>
+
+    <div class="form-floating">
+      <input type="password" name="confirmPasswd" class="form-control" id="confirmPassword" placeholder="Password">
+      <label for="confirmPassword">Confirm Password</label>
+    </div>
+
+    <button class="w-100 btn btn-lg btn-primary" name="updatePasswd" type="submit">Update</button>
+  </form>
+  <?php } else { ?>
+  <form method="POST" enctype="multipart/form-data">
+  <div class="mx-auto">
+  <img class="img-fluid w-100" src="<?php echo $this->rewritebase . $res[0]['photo']; ?>">
+    <h1 class="h3 mb-3 fw-normal"><?php echo $res[0]['nom']?></h1>
+
+    <div class="form-floating">
+      <input type="email" name="email" class="form-control" id="floatingInput" placeholder="name@example.com" style="background-image: url(&quot;data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAASCAYAAABSO15qAAAAAXNSR0IArs4c6QAAAPhJREFUOBHlU70KgzAQPlMhEvoQTg6OPoOjT+JWOnRqkUKHgqWP4OQbOPokTk6OTkVULNSLVc62oJmbIdzd95NcuGjX2/3YVI/Ts+t0WLE2ut5xsQ0O+90F6UxFjAI8qNcEGONia08e6MNONYwCS7EQAizLmtGUDEzTBNd1fxsYhjEBnHPQNG3KKTYV34F8ec/zwHEciOMYyrIE3/ehKAqIoggo9inGXKmFXwbyBkmSQJqmUNe15IRhCG3byphitm1/eUzDM4qR0TTNjEixGdAnSi3keS5vSk2UDKqqgizLqB4YzvassiKhGtZ/jDMtLOnHz7TE+yf8BaDZXA509yeBAAAAAElFTkSuQmCC&quot;); background-repeat: no-repeat; background-attachment: scroll; background-size: 16px 18px; background-position: 98% 50%;">
+      <label for="floatingInput">Email</label>
+    </div>
+    <div class="form-floating">
+      <a  href="<?php $this->rewritebase ?>profile/changePassword" name="passwd" class="btn btn-outline-primary" id="floatingPassword" placeholder="Password">Change password</a>
+    </div>
+    <div class="form-floating">
+          <input type="file" id="image" name="image" accept="image/*">
+    </div>
+
+    <button class="w-100 btn btn-lg btn-primary" name="updateProfile" type="submit">Update </button>
+    </div>
+  </form>
+  <?php } ?>
+</main>
