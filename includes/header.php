dfaea83826389e9116c191a2ac5f1c6217d444ce
Julien <julien.feliciano@qgandco.fr>
2021-10-23 10:05:34 +0200

    Minor changes on css + uploads

--- includes/header.php
+++ includes/header.php
@@ -6,9 +6,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="description" content="">
-  <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-  <meta name="generator" content="Hugo 0.83.1">
-  <title>Album example · Bootstrap v5.0</title>
+  <title>OC-Blog Project 5</title>
 
   <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/album/">
   <link href="<?= $this->rewritebase; ?>vendor/twbs/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -35,11 +33,7 @@
   <div class="navbar navbar-dark bg-dark shadow-sm">
     <div class="container">
       <a href="<?= $this->rewritebase; ?>" class="navbar-brand d-flex align-items-center">
-        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" aria-hidden="true" class="me-2" viewBox="0 0 24 24">
-          <path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z" />
-          <circle cx="12" cy="13" r="4" />
-        </svg>
-        <strong>Logo</strong>
+        <img class="logo" src="<?= $this->rewritebase; ?>uploads/logo.png">
       </a>
       <?php require_once 'nav.php' ?>
     </div>
